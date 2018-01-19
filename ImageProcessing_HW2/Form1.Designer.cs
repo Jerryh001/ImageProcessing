b5a459960f4dfd919d6872f3bce11e91dc500d08
Jerryh001 <jerryh001@hotmail.com>
2018-01-19 17:15:05 +0800

    HW2

---
+++ ImageProcessing_HW2/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace ImageProcessing_HW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.SplitContainer splitContainer1;
+            System.Windows.Forms.GroupBox groupBox8;
+            System.Windows.Forms.GroupBox groupBox7;
+            System.Windows.Forms.GroupBox groupBox6;
+            System.Windows.Forms.GroupBox groupBox5;
+            System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+            System.Windows.Forms.GroupBox groupBox4;
+            System.Windows.Forms.GroupBox groupBox3;
+            System.Windows.Forms.GroupBox groupBox2;
+            System.Windows.Forms.GroupBox groupBox1;
+            this.autoload = new System.Windows.Forms.CheckBox();
+            this.label_dc = new System.Windows.Forms.Label();
+            this.label_intersect = new System.Windows.Forms.Label();
+            this.label_gt = new System.Windows.Forms.Label();
+            this.label_result = new System.Windows.Forms.Label();
+            this.button_loadgt = new System.Windows.Forms.Button();
+            this.button_loadimage = new System.Windows.Forms.Button();
+            this.imageBox_gt = new Emgu.CV.UI.ImageBox();
+            this.imageBox_overlap = new Emgu.CV.UI.ImageBox();
+            this.imageBox_result = new Emgu.CV.UI.ImageBox();
+            this.imageBox_origin = new Emgu.CV.UI.ImageBox();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            splitContainer1 = new System.Windows.Forms.SplitContainer();
+            groupBox8 = new System.Windows.Forms.GroupBox();
+            groupBox7 = new System.Windows.Forms.GroupBox();
+            groupBox6 = new System.Windows.Forms.GroupBox();
+            groupBox5 = new System.Windows.Forms.GroupBox();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            groupBox4 = new System.Windows.Forms.GroupBox();
+            groupBox3 = new System.Windows.Forms.GroupBox();
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(splitContainer1)).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            groupBox8.SuspendLayout();
+            groupBox7.SuspendLayout();
+            groupBox6.SuspendLayout();
+            groupBox5.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_gt)).BeginInit();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_overlap)).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_result)).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_origin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer1.Location = new System.Drawing.Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(this.autoload);
+            splitContainer1.Panel1.Controls.Add(groupBox8);
+            splitContainer1.Panel1.Controls.Add(groupBox7);
+            splitContainer1.Panel1.Controls.Add(groupBox6);
+            splitContainer1.Panel1.Controls.Add(groupBox5);
+            splitContainer1.Panel1.Controls.Add(this.button_loadgt);
+            splitContainer1.Panel1.Controls.Add(this.button_loadimage);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(tableLayoutPanel1);
+            splitContainer1.Size = new System.Drawing.Size(1182, 853);
+            splitContainer1.SplitterDistance = 160;
+            splitContainer1.TabIndex = 0;
+            // 
+            // autoload
+            // 
+            this.autoload.AutoSize = true;
+            this.autoload.Location = new System.Drawing.Point(12, 328);
+            this.autoload.Name = "autoload";
+            this.autoload.Size = new System.Drawing.Size(108, 19);
+            this.autoload.TabIndex = 8;
+            this.autoload.Text = "自動載入GT";
+            this.autoload.UseVisualStyleBackColor = true;
+            // 
+            // groupBox8
+            // 
+            groupBox8.AutoSize = true;
+            groupBox8.Controls.Add(this.label_dc);
+            groupBox8.Location = new System.Drawing.Point(12, 272);
+            groupBox8.Name = "groupBox8";
+            groupBox8.Size = new System.Drawing.Size(140, 50);
+            groupBox8.TabIndex = 7;
+            groupBox8.TabStop = false;
+            groupBox8.Text = "D.C.";
+            // 
+            // label_dc
+            // 
+            this.label_dc.AutoSize = true;
+            this.label_dc.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label_dc.Location = new System.Drawing.Point(3, 21);
+            this.label_dc.Name = "label_dc";
+            this.label_dc.Size = new System.Drawing.Size(14, 15);
+            this.label_dc.TabIndex = 0;
+            this.label_dc.Text = "0";
+            // 
+            // groupBox7
+            // 
+            groupBox7.AutoSize = true;
+            groupBox7.Controls.Add(this.label_intersect);
+            groupBox7.Location = new System.Drawing.Point(12, 216);
+            groupBox7.Name = "groupBox7";
+            groupBox7.Size = new System.Drawing.Size(140, 50);
+            groupBox7.TabIndex = 6;
+            groupBox7.TabStop = false;
+            groupBox7.Text = "Intersect";
+            // 
+            // label_intersect
+            // 
+            this.label_intersect.AutoSize = true;
+            this.label_intersect.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label_intersect.Location = new System.Drawing.Point(3, 21);
+            this.label_intersect.Name = "label_intersect";
+            this.label_intersect.Size = new System.Drawing.Size(14, 15);
+            this.label_intersect.TabIndex = 0;
+            this.label_intersect.Text = "0";
+            // 
+            // groupBox6
+            // 
+            groupBox6.AutoSize = true;
+            groupBox6.Controls.Add(this.label_gt);
+            groupBox6.Location = new System.Drawing.Point(12, 160);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new System.Drawing.Size(140, 50);
+            groupBox6.TabIndex = 5;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "G.T.";
+            // 
+            // label_gt
+            // 
+            this.label_gt.AutoSize = true;
+            this.label_gt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label_gt.Location = new System.Drawing.Point(3, 21);
+            this.label_gt.Name = "label_gt";
+            this.label_gt.Size = new System.Drawing.Size(14, 15);
+            this.label_gt.TabIndex = 0;
+            this.label_gt.Text = "0";
+            // 
+            // groupBox5
+            // 
+            groupBox5.AutoSize = true;
+            groupBox5.Controls.Add(this.label_result);
+            groupBox5.Location = new System.Drawing.Point(12, 104);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new System.Drawing.Size(140, 50);
+            groupBox5.TabIndex = 4;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Result";
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label_result.Location = new System.Drawing.Point(3, 21);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(14, 15);
+            this.label_result.TabIndex = 0;
+            this.label_result.Text = "0";
+            // 
+            // button_loadgt
+            // 
+            this.button_loadgt.Location = new System.Drawing.Point(12, 58);
+            this.button_loadgt.Name = "button_loadgt";
+            this.button_loadgt.Size = new System.Drawing.Size(140, 40);
+            this.button_loadgt.TabIndex = 1;
+            this.button_loadgt.Text = "載入G.T.";
+            this.button_loadgt.UseVisualStyleBackColor = true;
+            this.button_loadgt.Click += new System.EventHandler(this.button_loadgt_Click);
+            // 
+            // button_loadimage
+            // 
+            this.button_loadimage.Location = new System.Drawing.Point(12, 12);
+            this.button_loadimage.Name = "button_loadimage";
+            this.button_loadimage.Size = new System.Drawing.Size(140, 40);
+            this.button_loadimage.TabIndex = 0;
+            this.button_loadimage.Text = "載入圖檔";
+            this.button_loadimage.UseVisualStyleBackColor = true;
+            this.button_loadimage.Click += new System.EventHandler(this.button_loadimage_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(groupBox4, 1, 1);
+            tableLayoutPanel1.Controls.Add(groupBox3, 0, 1);
+            tableLayoutPanel1.Controls.Add(groupBox2, 1, 0);
+            tableLayoutPanel1.Controls.Add(groupBox1, 0, 0);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(1018, 853);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(this.imageBox_gt);
+            groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            groupBox4.Location = new System.Drawing.Point(512, 429);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new System.Drawing.Size(503, 421);
+            groupBox4.TabIndex = 3;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "G.T.";
+            // 
+            // imageBox_gt
+            // 
+            this.imageBox_gt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox_gt.Location = new System.Drawing.Point(3, 21);
+            this.imageBox_gt.Name = "imageBox_gt";
+            this.imageBox_gt.Size = new System.Drawing.Size(497, 397);
+            this.imageBox_gt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox_gt.TabIndex = 3;
+            this.imageBox_gt.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(this.imageBox_overlap);
+            groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            groupBox3.Location = new System.Drawing.Point(3, 429);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new System.Drawing.Size(503, 421);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "疊加結果";
+            // 
+            // imageBox_overlap
+            // 
+            this.imageBox_overlap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox_overlap.Location = new System.Drawing.Point(3, 21);
+            this.imageBox_overlap.Name = "imageBox_overlap";
+            this.imageBox_overlap.Size = new System.Drawing.Size(497, 397);
+            this.imageBox_overlap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox_overlap.TabIndex = 3;
+            this.imageBox_overlap.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(this.imageBox_result);
+            groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            groupBox2.Location = new System.Drawing.Point(512, 3);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new System.Drawing.Size(503, 420);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "計算結果";
+            // 
+            // imageBox_result
+            // 
+            this.imageBox_result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox_result.Location = new System.Drawing.Point(3, 21);
+            this.imageBox_result.Name = "imageBox_result";
+            this.imageBox_result.Size = new System.Drawing.Size(497, 396);
+            this.imageBox_result.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox_result.TabIndex = 3;
+            this.imageBox_result.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(this.imageBox_origin);
+            groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            groupBox1.Location = new System.Drawing.Point(3, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(503, 420);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "原始圖片";
+            // 
+            // imageBox_origin
+            // 
+            this.imageBox_origin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox_origin.Location = new System.Drawing.Point(3, 21);
+            this.imageBox_origin.Name = "imageBox_origin";
+            this.imageBox_origin.Size = new System.Drawing.Size(497, 396);
+            this.imageBox_origin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox_origin.TabIndex = 2;
+            this.imageBox_origin.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 853);
+            this.Controls.Add(splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(splitContainer1)).EndInit();
+            splitContainer1.ResumeLayout(false);
+            groupBox8.ResumeLayout(false);
+            groupBox8.PerformLayout();
+            groupBox7.ResumeLayout(false);
+            groupBox7.PerformLayout();
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_gt)).EndInit();
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_overlap)).EndInit();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_result)).EndInit();
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_origin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_loadgt;
+        private System.Windows.Forms.Button button_loadimage;
+        private Emgu.CV.UI.ImageBox imageBox_gt;
+        private Emgu.CV.UI.ImageBox imageBox_overlap;
+        private Emgu.CV.UI.ImageBox imageBox_result;
+        private Emgu.CV.UI.ImageBox imageBox_origin;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.Label label_gt;
+        private System.Windows.Forms.Label label_intersect;
+        private System.Windows.Forms.Label label_dc;
+        private System.Windows.Forms.CheckBox autoload;
+    }
+}
+
---
+++ ImageProcessing_HW2/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace ImageProcessing_HW2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button_loadimage_Click(object sender, EventArgs e)
+        {
+            if(openFile.ShowDialog()==DialogResult.OK)
+            {
+                imageBox_origin.Image = new Image<Bgr, byte>(openFile.FileName);
+                if(autoload.Checked)
+                {
+                    try
+                    {
+                        imageBox_gt.Image = new Image<Gray, byte>(openFile.FileName.Replace("\\Data\\", "\\GT\\"));
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                Run();
+            }
+        }
+
+        private void button_loadgt_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                imageBox_gt.Image = new Image<Gray, byte>(openFile.FileName);
+                if(imageBox_origin.Image!=null)
+                {
+                    overlapDraw();
+                }
+            }
+        }
+        private void Run()
+        {
+            
+            Image<Bgr, byte> origin_bgr = (Image<Bgr, byte>)imageBox_origin.Image;
+
+            Image<Gray, byte>[] split_bgr = origin_bgr.Split();
+            Image<Gray, byte> green = origin_bgr[1];
+            Image<Gray, byte> blue = origin_bgr[0];
+
+            green._EqualizeHist();
+            blue._EqualizeHist();
+
+            Image<Hsv, byte> origin_hsv = origin_bgr.Convert<Hsv, byte>();
+
+            Image<Gray, byte> saturation = origin_hsv.Split()[1];
+
+            saturation._EqualizeHist();
+
+            int step = 5;
+
+            Image<Gray, byte> sred = saturation.Max(saturation.Not());
+            sred._EqualizeHist();
+
+            Image<Gray, byte> t1 = sred.ThresholdBinary(new Gray(192), new Gray(255)).Dilate(step).Erode(step).Erode(step).Dilate(step);
+            Image<Gray, byte> t2 = blue.Or(green).Not();
+            t2._EqualizeHist();
+            Image<Gray, byte> t3 = t2.ThresholdBinary(new Gray(192), new Gray(255)).Dilate(step).Erode(step).Erode(step).Dilate(step);
+
+            Image<Gray, byte> result = t1.And(t3).Dilate(step).Erode(step).Erode(step).Dilate(step);
+
+            using (VectorOfVectorOfPoint contours = FindContours(result))
+            {
+                Tuple<int, double> max = FindMaxArea(contours,result.Size);
+                imageBox_result.Image = new Image<Gray,byte>(result.Size).Not();
+                CvInvoke.DrawContours(imageBox_result.Image, contours, max.Item1, new MCvScalar(0, 0, 0), -1);
+            }
+            overlapDraw();
+        }
+        private VectorOfVectorOfPoint FindContours(IInputOutputArray image)
+        {
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(image, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+            return contours;
+        }
+        private Tuple<int, double> FindMaxArea(VectorOfVectorOfPoint contours,Size size)
+        {
+            if (contours.Size == 0) return new Tuple<int, double>(-1, 0);
+            Tuple<int, double> max = new Tuple<int, double>(-1, 0);
+            for (int i = 0; i < contours.Size; i++)
+            {
+                double s = CvInvoke.ContourArea(contours[i]);
+                if (max.Item1<0||s > max.Item2)
+                {
+                    max = new Tuple<int, double>(i, s);
+                }
+            }
+            return max;
+        }
+        private void overlapDraw()
+        {
+            imageBox_overlap.Image = (Image<Bgr, byte>)imageBox_origin.Image.Clone();
+            if(imageBox_result.Image!=null)
+            {
+                using (VectorOfVectorOfPoint contours = FindContours(((Image<Gray, byte>)imageBox_result.Image).Not()))
+                {
+                    Tuple<int, double> max = FindMaxArea(contours, imageBox_result.Image.Size);
+                    label_result.Text = "0";
+                    if (max.Item1>=0)
+                    {
+                        label_result.Text = max.Item2.ToString("0.##");
+                        CvInvoke.DrawContours(imageBox_overlap.Image, contours, max.Item1, new MCvScalar(0, 0, 255), 3);
+                    }
+                }
+            }
+            if (imageBox_gt.Image != null)
+            {
+                using (VectorOfVectorOfPoint contours = FindContours(((Image<Gray, byte>)imageBox_gt.Image).Not()))
+                {
+                    Tuple<int, double> max = FindMaxArea(contours, imageBox_gt.Image.Size);
+                    label_gt.Text = "0";
+                    if (max.Item1 >= 0)
+                    {
+                        label_gt.Text = max.Item2.ToString("0.##");
+                        CvInvoke.DrawContours(imageBox_overlap.Image, contours, max.Item1, new MCvScalar(0, 255, 0), 3);
+                    }
+                }
+            }
+            imageBox_overlap.Refresh();
+            if(label_result.Text != "0"&& label_gt.Text != "0")
+            {
+                Image<Gray, byte> imageand = ((Image<Gray, byte>)imageBox_result.Image).Or((Image<Gray, byte>)imageBox_gt.Image);
+                using (VectorOfVectorOfPoint contours = FindContours(imageand.Not()))
+                {
+                    Tuple<int, double> max = FindMaxArea(contours, imageand.Size);
+                    label_intersect.Text = "0";
+                    if (max.Item1 >= 0)
+                    {
+                        label_intersect.Text = max.Item2.ToString("0.##");
+                        label_dc.Text = (2 * Convert.ToDouble(label_intersect.Text) / (Convert.ToDouble(label_result.Text) + Convert.ToDouble(label_gt.Text))).ToString("0.00");
+                    }
+                }
+            }
+        }
+    }
+}
